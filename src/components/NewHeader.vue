127157075d59d7d98c8a4dbb5b420ca7ce1a8cef
mangoyi <mangoyiy@163.com>
2018-04-09 11:47:01 +0800

    NavHeader

--- src/components/NewHeader.vue
+++ src/components/NewHeader.vue
@@ -14,15 +14,249 @@
                     <li><a href="#">首页</a></li>
                     <li>
                         <a href="#">方案</a>
-                        <div>
-                            
+                        <div class="h-list h-solution">
+                            <div class="h-l-wrap">
+                                <dl class="d1">
+                                    <dt>按应用场景</dt>
+                                    <dl class="d1-2">
+                                        <dt><a href="#">优质高效的电话沟通方案</a></dt>
+                                        <dd><a href="#">大规模门店电话沟通集中管理</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">接入运营商IMS核心网</a></dt>
+                                        <dd><a href="">30B+D接入方案</a></dd>
+                                        <dd><a href="#">ONU接入方案</a></dd>
+                                        <dd><a href="#">SIP中继对接IP-PBX</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">企业电话录音解决方案</a></dt>
+                                    </dl>
+                                </dl>
+                                <dl class="d2">
+                                    <dt>按功能</dt>
+                                    <dd><a href="#">智能值班</a></dd>
+                                    <dd><a href="#">VIP专门服务</a></dd>
+                                    <dd><a href="#">FXO</a></dd>
+                                </dl>
+                                <dl class="d3">
+                                    <dt>按行业</dt>
+                                    <dd><a href="#">船舶卫星</a></dd>
+                                </dl>
+                            </div>
                         </div>    
                     </li>
-                    <li><a href="#">产品</a></li>
-                    <li><a href="#">技术支持</a></li>
-                    <li><a href="#">合作伙伴</a></li>
-                    <li><a href="#">应用平台</a></li>
-                    <li><a href="#">关于我们</a></li>
+                    <li>
+                        <a href="#">产品</a>
+                        <div class="h-list h-solution">
+                            <div class="h-l-wrap">
+                                <dl class="d1">
+                                    <dt>按应用场景</dt>
+                                    <dl class="d1-2">
+                                        <dt><a href="#">优质高效的电话沟通方案</a></dt>
+                                        <dd><a href="">客户来电不漏接</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">IP网络上部署电话系统</a></dt>
+                                        <dd><a href="#">无电话线办公环境</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dd><a href="#">ONU接入方案</a></dd>
+                                        <dd><a href="#">SIP中继对接IP-PBX</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">企业电话录音解决方案</a></dt>
+                                    </dl>
+                                </dl>
+                                <dl class="d2">
+                                    <dt>按功能</dt>
+                                    <dd><a href="#">智能值班</a></dd>
+                                    <dd><a href="#">VIP专门服务</a></dd>
+                                    <dd><a href="#">逃生</a></dd>
+                                    <dd><a href="#">穿透服务</a></dd>
+                                    <dd><a href="#">FXO</a></dd>
+                                </dl>
+                                <dl class="d3">
+                                    <dt>按行业</dt>
+                                    <dd><a href="#">船舶卫星</a></dd>
+                                    <dd><a href="#">监狱电话</a></dd>
+                                    <dd><a href="#">智慧养老</a></dd>
+                                    <dd><a href="#">隧道电话</a></dd>
+                                    <dd><a href="#">门店电话管理</a></dd>
+                                    <dd><a href="#">云通信</a></dd>
+                                    <dd><a href="#">经济型酒店</a></dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">技术支持</a>
+                        <div class="h-list h-solution">
+                            <div class="h-l-wrap">
+                                <dl class="d1">
+                                    <dt>按应用场景</dt>
+                                    <dl class="d1-2">
+                                        <dd><a href="#">大规模门店电话沟通集中管理</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">IP网络上部署电话系统</a></dt>
+                                        <dd><a href="">企业跨区域电话组网</a></dd>
+                                        <dd><a href="#">厂区园区电话部署</a></dd>
+                                        <dd><a href="#">无电话线办公环境</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">接入运营商IMS核心网</a></dt>
+                                        <dd><a href="">30B+D接入方案</a></dd>
+                                        <dd><a href="#">ONU接入方案</a></dd>
+                                        <dd><a href="#">SIP中继对接IP-PBX</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">企业电话录音解决方案</a></dt>
+                                    </dl>
+                                </dl>
+                                <dl class="d2">
+                                    <dt>按功能</dt>
+                                    <dd><a href="#">智能值班</a></dd>
+                                    <dd><a href="#">VIP专门服务</a></dd>
+                                    <dd><a href="#">逃生</a></dd>
+                                    <dd><a href="#">穿透服务</a></dd>
+                                    <dd><a href="#">FXO</a></dd>
+                                </dl>
+                                <dl class="d3">
+                                    <dt>按行业</dt>
+                                    <dd><a href="#">船舶卫星</a></dd>
+                                    <dd><a href="#">监狱电话</a></dd>
+                                    <dd><a href="#">智慧养老</a></dd>
+                                    <dd><a href="#">隧道电话</a></dd>
+                                    <dd><a href="#">门店电话管理</a></dd>
+                                    <dd><a href="#">云通信</a></dd>
+                                    <dd><a href="#">经济型酒店</a></dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">合作伙伴</a>
+                        <div class="h-list h-solution">
+                            <div class="h-l-wrap">
+                                <dl class="d1">
+                                    <dt>按应用场景</dt>
+                                    <dl class="d1-2">
+                                        <dt><a href="#">优质高效的电话沟通方案</a></dt>
+                                        <dd><a href="#">一体化小型呼叫中心</a></dd>
+                                        <dd><a href="#">大规模门店电话沟通集中管理</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dd><a href="#">厂区园区电话部署</a></dd>
+                                        <dd><a href="#">无电话线办公环境</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">接入运营商IMS核心网</a></dt>
+                                        <dd><a href="#">SIP中继对接IP-PBX</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">企业电话录音解决方案</a></dt>
+                                    </dl>
+                                </dl>
+                                <dl class="d2">
+                                    <dt>按功能</dt>
+                                    <dd><a href="#">智能值班</a></dd>
+                                    <dd><a href="#">VIP专门服务</a></dd>
+                                    <dd><a href="#">逃生</a></dd>
+                                    <dd><a href="#">穿透服务</a></dd>
+                                    <dd><a href="#">FXO</a></dd>
+                                </dl>
+                                <dl class="d3">
+                                    <dt>按行业</dt>
+                                    <dd><a href="#">船舶卫星</a></dd>
+                                    <dd><a href="#">监狱电话</a></dd>
+                                    <dd><a href="#">智慧养老</a></dd>
+                                    <dd><a href="#">隧道电话</a></dd>
+                                    <dd><a href="#">门店电话管理</a></dd>
+                                    <dd><a href="#">云通信</a></dd>
+                                    <dd><a href="#">经济型酒店</a></dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">应用平台</a>
+                        <div class="h-list h-solution">
+                            <div class="h-l-wrap">
+                                <dl class="d1">
+                                    <dt>按应用场景</dt>
+                                    <dl class="d1-2">
+                                        <dt><a href="#">优质高效的电话沟通方案</a></dt>
+                                        <dd><a href="">客户来电不漏接</a></dd>
+                                        <dd><a href="#">大规模门店电话沟通集中管理</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">IP网络上部署电话系统</a></dt>
+                                        <dd><a href="#">厂区园区电话部署</a></dd>
+                                        <dd><a href="#">无电话线办公环境</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">接入运营商IMS核心网</a></dt>
+                                        <dd><a href="">30B+D接入方案</a></dd>
+                                        <dd><a href="#">ONU接入方案</a></dd>
+                                        <dd><a href="#">SIP中继对接IP-PBX</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">企业电话录音解决方案</a></dt>
+                                    </dl>
+                                </dl>
+                                <dl class="d2">
+                                    <dt>按功能</dt>
+                                    <dd><a href="#">智能值班</a></dd>
+                                    <dd><a href="#">VIP专门服务</a></dd>
+                                    <dd><a href="#">穿透服务</a></dd>
+                                    <dd><a href="#">FXO</a></dd>
+                                </dl>
+                                <dl class="d3">
+                                    <dt>按行业</dt>
+                                    <dd><a href="#">船舶卫星</a></dd>
+                                    <dd><a href="#">云通信</a></dd>
+                                    <dd><a href="#">经济型酒店</a></dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">关于我们</a>
+                        <div class="h-list h-solution">
+                            <div class="h-l-wrap">
+                                <dl class="d1">
+                                    <dt>按应用场景</dt>
+                                    <dl class="d1-2">
+                                        <dt><a href="#">优质高效的电话沟通方案</a></dt>
+                                        <dd><a href="">客户来电不漏接</a></dd>
+                                        <dd><a href="#">一体化小型呼叫中心</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">IP网络上部署电话系统</a></dt>
+                                        <dd><a href="#">厂区园区电话部署</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">接入运营商IMS核心网</a></dt>
+                                        <dd><a href="">30B+D接入方案</a></dd>
+                                        <dd><a href="#">SIP中继对接IP-PBX</a></dd>
+                                    </dl>
+                                    <dl  class="d1-2">
+                                        <dt><a href="#">企业电话录音解决方案</a></dt>
+                                    </dl>
+                                </dl>
+                                <dl class="d2">
+                                    <dt>按功能</dt>
+                                    <dd><a href="#">智能值班</a></dd>
+                                    <dd><a href="#">逃生</a></dd>
+                                    <dd><a href="#">穿透服务</a></dd>
+                                    <dd><a href="#">FXO</a></dd>
+                                </dl>
+                                <dl class="d3">
+                                    <dt>按行业</dt>
+                                </dl>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -30,9 +264,10 @@
 </template>
 
 <script>
+
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
 @import "./../assets/css/reset.css";
 
 .navwrap {
@@ -79,7 +314,7 @@
 .navul li {
     padding: 0 20px;
 }
-.navul li a {
+.navul li>a {
     display: block;
     text-decoration: none;
     color: #777;
@@ -87,11 +322,77 @@
     height: 60px;
     line-height: 60px;
 }
+.navul li>a:hover {
+    border-bottom: 2px solid #003D88;
+}
 .navul li:first-child {
     padding-left: 0;
 }
 .navul li:last-child {
     padding-right: 0;
+    }
+.navul li a:hover {
+    color: #003D88;
+}
+.navul li:hover .h-list {
+    display: block;
+}
+.navul li .h-list {
+    display: none;
+    position: absolute;
+    width: 100%;
+    left: 0;
+    top: 92px;
+    background: rgba(235, 236, 238, 0.4);
+}
+.navul .h-l-wrap {
+    min-width: 1200px;
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+}
+
+.navul .h-l-wrap .d1 {
+    width: 900px;
+}
+.navul .h-l-wrap .d2 {
+    width: 120px;
+
+}
+.navul .h-l-wrap .d3 {
+    width: 100px;
+}
+.navul .h-l-wrap  dt {
+    margin: 10px 0;
+}
+.navul .h-l-wrap  .d1-2 {
+    float: left ;
+}
+.navul .h-l-wrap  .d1-2 {
+    width: 210px;
+}
+.navul .h-l-wrap  .d1-2 dt a:hover {
+    text-decoration: underline;
+}
+.navul .h-l-wrap  .d1-2 dd a , .navul .h-l-wrap .d2 dd a, .navul .h-l-wrap .d3 dd a{
+    border-bottom: 0;
+    display: inline-block;
+    height: auto;
+    line-height: 32px;
+    font-size: 16px;
+    color: #333;
+    text-decoration: none;
+}
+.navul .h-l-wrap  .d1-2 dd a:hover , .navul .h-l-wrap .d2 dd a:hover, .navul .h-l-wrap .d3 dd a:hover{
+    color: #003d88;
+    text-decoration: underline;
+}
+.navul .h-l-wrap dl dt, .navul .h-l-wrap  .d1-2 dt a{
+    font-size: 18px;
+    color: #003D88;
+}
+.navul .h-l-wrap .d1-2 dt a{
+    text-decoration: none;
 }
 
 </style>
